a99e7bb2e0f62550c457eef6474def461d3fb04a
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2015-05-08 03:12:37 +0300

    version fix

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ OpenSSL_Extension = Extension (
 
 setup (
     name='coro',
-    version='1.0.3-000-p03',
+    version='1.0.3-000-p04',
     description='IronPort Coroutine/Threading Library',
     author='Sam Rushing, Eric Huss, IronPort Engineering',
     author_email='sam-coro@rushing.nightmare.com',
